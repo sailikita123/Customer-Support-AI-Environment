042e855535241c65de0823a3fd3d6b24fb50b86a
sailikita123 <sailikitas@gmail.com>
2026-04-02 19:52:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from inference import run_inference
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "Customer Support AI Running 🚀"}
@@ -18,3 +19,20 @@ def run():
 @app.get("/health")
 def health():
     return {"status": "OK"}
+
+
+@app.post("/reset")
+def reset():
+    return {
+        "status": "reset successful"
+    }
+
+
+
+@app.post("/step")
+def step():
+    result = run_inference()
+    return {
+        "status": "step executed",
+        "result": result
+    }
